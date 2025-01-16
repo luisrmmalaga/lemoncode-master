7f506d9517a8a8e1a70cf33dd84b85144f380df3
luisrm <luisrmmalaga@gmail.com>
2025-01-16 18:53:11 +0100

    Sesion 2 bundling

--- laboratorio-m3-bundling/sesion-2/src/index.js
+++ laboratorio-m3-bundling/sesion-2/src/index.js
@@ -1,6 +1,6 @@
 import { getAverage } from "./average-service.js";
-import "./mystyles.scss";
 import imgRio from "./content/rio.png";
+import "./mystyles.scss";
 
 const average = getAverage([1, 2, 3, 4, 5]);
 const message2 = `El promedio es: ${average}`;
