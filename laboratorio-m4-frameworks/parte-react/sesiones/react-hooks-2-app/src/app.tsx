ed02b82425385d53ac205d25d7eb8eba5dfc4053
luisrm <luisrmmalaga@gmail.com>
2025-02-13 18:47:59 +0100

    React - sesion 3

---
+++ laboratorio-m4-frameworks/parte-react/sesiones/react-hooks-2-app/src/app.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export const App = () => {
+  // --------------useRef example-----------------------
+  // const [count, setCount] = React.useState(0);
+  // const render = React.useRef(0);
+
+  // render.current = render.current + 1;
+
+  // React.useEffect(() => {
+  //   setInterval(() => {
+  //     console.log("interval tick");
+  //     render.current = render.current + 1;
+  //   }, 3000);
+  // }, []);
+
+  // const handleClick = () => {
+  //   setCount(count + 1);
+  // };
+
+  // console.log("Rerender");
+  //-----------------------------------------------------
+
+  return (
+    // <> ------------  useRef example -----------
+    //   <button onClick={handleClick}>Clicked {count} times</button>
+    //   <p>Renders: {render.current}</p>
+    // </> ------------------------------------------
+    <></>
+  );
+};
