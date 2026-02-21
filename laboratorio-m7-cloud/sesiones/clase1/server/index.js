defa3e8e0cdc41f34568658384e1f803a2f7de23
luisrmmalaga <luisrmmalaga@gmail.com>
2026-02-22 00:38:51 +0100

    Clase 1 módulo cloud

---
+++ laboratorio-m7-cloud/sesiones/clase1/server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const staticFilesPath = path.resolve(__dirname, './public');
+app.use('/', express.static(staticFilesPath));
+
+// my-app.com/list
+app.get(/(.*)/, (req, res) => {
+  res.sendFile(path.resolve(staticFilesPath, 'index.html'));
+});
+
+const PORT = process.env.PORT || 8081;
+app.listen(PORT, () => {
+  console.log(`App running on http://localhost:${PORT}`);
+});
